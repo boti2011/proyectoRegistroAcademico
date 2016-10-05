3214bcc9cf39bc2634caa28d9e53f293ed73ef6a
LT-BCSB\brian_000 <bcamilo@gmail.com>
2016-10-04 19:17:57 -0500

    Primer Commit - Proyecto Registro Académico

---
+++ src/main/webapp/app/components/form/pagination.constants.js
@@ -0,0 +1,9 @@
+(function() {
+    'use strict';
+
+    angular
+        .module('proyectoRegistroAcademicoApp')
+        .constant('paginationConstants', {
+            'itemsPerPage': 20
+        });
+})();
